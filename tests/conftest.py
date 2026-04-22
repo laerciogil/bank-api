2873fb060c448cd566ef0a105471ef1e5b7b17bd
Laercio Gil <laerciogilpb@gmail.com>
2026-04-22 00:36:01 -0300

    feat: add initial test fixtures and a basic test case

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from bank_api.main import app as _app
+
+
+@pytest.fixture(scope="module")
+def app():
+    """Instance of main app"""
+    return _app
+
+@pytest.fixture(scope="module")
+def client():
+    """Instance of Client"""
+    return TestClient(app)
---
+++ tests/test_app.py
@@ -0,0 +1,5 @@
+
+
+def test_basic():
+    response = 1
+    assert response == 3-2
