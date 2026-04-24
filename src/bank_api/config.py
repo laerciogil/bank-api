28d6ead6651c19fe3733ada004b87679f0288c83
Laercio Gil <laerciogilpb@gmail.com>
2026-04-24 02:27:02 -0300

    feat: standardize settings reading from environment variables

--- src/bank_api/config.py
+++ src/bank_api/config.py
@@ -4,16 +4,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
+    environment: str = os.getenv("ENVIRONMENT", "DEV")
+    database_url: str = os.getenv("DATABASE_URL", "")
+    auth_secrete_key: str = os.getenv("AUTH_SECRETE_KEY", "")
+
+    def __init__(self, model_config: SettingsConfigDict) -> None:
+        super().__init__(model_config=model_config)
+
+
+settings = Settings(
+    model_config=SettingsConfigDict(
         env_file=".env",
         extra="forbid",
         env_file_encoding="utf-8",
-    )
-    environment: str = "PROD"
-    database_url: str = os.getenv("DATABASE_URL", "")
-    auth_secrete_key: str = os.getenv(
-        "AUTH_SECRETE_KEY", "mysecretkey-with-a-minimum-length-of-32-characters",
-    )
-
-
-settings = Settings()
+    ),
+)
