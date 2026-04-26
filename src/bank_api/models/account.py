df29e1f108fd3ada60fc99a7bff1d3f26473127b
Laercio Gil <laerciogilpb@gmail.com>
2026-04-26 05:01:27 -0300

    fix: remove fk reference to an inexistent user table

--- src/bank_api/models/account.py
+++ src/bank_api/models/account.py
@@ -6,7 +6,7 @@ accounts = sa.Table(
     "account",
     metadata,
     sa.Column("id", sa.Integer, primary_key=True),
-    sa.Column("user_id", sa.Integer, sa.ForeignKey("user.id"), nullable=False),
+    sa.Column("user_id", sa.Integer, nullable=False),
     sa.Column("balance", sa.Numeric(10, 2), nullable=False),
     sa.Column("created_at", sa.TIMESTAMP(timezone=True), default=sa.func.now()),
 )
