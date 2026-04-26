b7a1232d26c20fb6d70de209377c0fb12838a385
Laercio Gil <laerciogilpb@gmail.com>
2026-04-26 04:59:17 -0300

    feat: register new account controller router to main app

--- src/bank_api/main.py
+++ src/bank_api/main.py
@@ -5,7 +5,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from .controllers import auth, root
+from .controllers import account, auth, root
 from .database import database, engine, metadata
 
 if TYPE_CHECKING:
@@ -38,6 +38,7 @@ app.add_middleware(
 
 app.include_router(root.router)
 app.include_router(auth.router)
+app.include_router(account.router)
 
 
 def run() -> None:
