a47bdcfd5e0233e7d6c1f295687c8de8a4923286
Laercio Gil <laerciogilpb@gmail.com>
2026-04-26 18:32:27 -0300

    feat: create new controller for transaction endpoints

---
+++ src/bank_api/controllers/transaction.py
@@ -0,0 +1,26 @@
+from typing import TYPE_CHECKING
+
+from fastapi import APIRouter, Depends, status
+
+from bank_api.security import login_required
+from bank_api.service.transaction import TransactionService
+from bank_api.views.transaction import TransactionResponse
+
+if TYPE_CHECKING:
+    from bank_api.schemas.transaction import TransactionRequest
+
+router = APIRouter(
+    prefix="/transactions",
+    tags=["Transaction"],
+    dependencies=[Depends(login_required)],
+)
+
+transaction_service = TransactionService()
+
+@router.post(
+    "",
+    response_model=TransactionResponse,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_transaction(transaction: TransactionRequest) -> TransactionResponse:
+    return await transaction_service.create(transaction=transaction)
