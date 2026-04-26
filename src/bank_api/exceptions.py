27d2d27bfce71f9a6cf8227054bf1f4901bdeb1f
Laercio Gil <laerciogilpb@gmail.com>
2026-04-26 04:18:53 -0300

    feat: create InsufficientFundsError exception class

--- src/bank_api/exceptions.py
+++ src/bank_api/exceptions.py
@@ -1,5 +1,12 @@
 class AccountNotFoundError(Exception):
     pass
 
+
 class BusinessError(Exception):
     pass
+
+
+class InsufficientFundsError(Exception):
+    def __init__(self, funds: float) -> None:
+        self.funds = funds
+        super().__init__(f"Not enough funds. Available: {funds}")
